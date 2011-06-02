eeb653af12221f1890797ff0463d1cd683cbf0fe
Matt Honeycutt <mbhoneycutt@gmail.com>
2011-06-01 23:09:02 -0500

    Validation stuff removed

--- FailTracker.Web/Models/Account/LogOnForm.cs
+++ FailTracker.Web/Models/Account/LogOnForm.cs
@@ -1,17 +1,13 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.Web.Mvc;
 
 namespace FailTracker.Web.Models.Account
 {
 	public class LogOnForm
 	{
-		[Required]
 		[DisplayName("Email Address")]
-		[EmailAddress]
 		public string EmailAddress { get; set; }
 
-		[Required]
 		[DataType(DataType.Password)]
 		public string Password { get; set; }
 	}
